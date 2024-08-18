c26e4547099e765c41e3d6e05756c0a6e7c446af
duonglh <duonglh@rocketstudio.com.vn>
2024-08-19 02:24:56 +0700

    time frame for the attacks

---
+++ Assets/Scripts/Runtime/Player/States/GroundStates/AttackPS.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using Z3.GMTK2024.States;
+using Z3.NodeGraph.Core;
+using Z3.Utils.ExtensionMethods;
+
+namespace Z3.GMTK2024
+{
+    public class AttackPS : CharacterAction
+    {
+        [SerializeField] private Parameter<Animator> animator;
+        [SerializeField] private Parameter<float[]> transitions;
+
+        private string[] attackAnimationHashed = new[]
+        {
+            "Attack_01",
+            "Attack_02",
+            "Attack_03"
+        };
+
+        private int currentAttackId = -1;
+        private float lastAttackTime = float.NegativeInfinity;
+
+        protected override void StartAction()
+        {
+            base.StartAction();
+            DecideAttackId();
+
+            animator.Value.PlayState(attackAnimationHashed[currentAttackId], transitions.Value[currentAttackId]);
+            lastAttackTime = Time.time;
+            EndAction();
+        }
+
+        private void DecideAttackId()
+        {
+            if (currentAttackId < 0)
+            {
+                currentAttackId = 0;
+                return;
+            }
+
+            var timePassedSinceLastAttack = Time.time - lastAttackTime;
+            var timeFrame = Data.AttackTimeFrames[currentAttackId];
+            if (timePassedSinceLastAttack < timeFrame)
+            {
+                currentAttackId++;
+                currentAttackId %= attackAnimationHashed.Length;
+
+                return;
+            }
+
+            currentAttackId = 0;
+        }
+    }
+}
