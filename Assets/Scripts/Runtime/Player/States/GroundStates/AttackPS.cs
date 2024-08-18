1550a9cde0c2881f14af37a913f162115e743705
duonglh <duonglh@rocketstudio.com.vn>
2024-08-19 02:36:15 +0700

    merge the animationEnded logic with AttackPS as it is easier to add Attack logic in AttackPS this way

--- Assets/Scripts/Runtime/Player/States/GroundStates/AttackPS.cs
+++ Assets/Scripts/Runtime/Player/States/GroundStates/AttackPS.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Z3.GMTK2024.Shared;
 using Z3.GMTK2024.States;
 using Z3.NodeGraph.Core;
 using Z3.Utils.ExtensionMethods;
@@ -10,6 +13,11 @@ namespace Z3.GMTK2024
         [SerializeField] private Parameter<Animator> animator;
         [SerializeField] private Parameter<float[]> transitions;
 
+        [ParameterDefinition(AutoBindType.SelfBind)] [SerializeField]
+        private Parameter<AnimationEventTrigger> animationEventTrigger;
+
+        [SerializeField] private Parameter<string> eventName;
+
         private string[] attackAnimationHashed = new[]
         {
             "Attack_01",
@@ -19,15 +27,50 @@ namespace Z3.GMTK2024
 
         private int currentAttackId = -1;
         private float lastAttackTime = float.NegativeInfinity;
+        private bool animationEnded;
 
         protected override void StartAction()
         {
             base.StartAction();
+            animationEnded = false;
+            animationEventTrigger.Value.OnEventTrigger += OnEventTrigger;
+
             DecideAttackId();
 
             animator.Value.PlayState(attackAnimationHashed[currentAttackId], transitions.Value[currentAttackId]);
             lastAttackTime = Time.time;
-            EndAction();
+        }
+
+        protected override void UpdateAction()
+        {
+            if (animationEnded)
+            {
+                EndAction();
+                return;
+            }
+
+            base.UpdateAction();
+        }
+
+        protected override void StopAction()
+        {
+            base.StopAction();
+            if (!animationEnded)
+            {
+                // The attack was interrupted before it ends
+                // Stop the animation and any damage related logic here
+                // Note: the animatedEnded variable can be changed to '!canDealDamage' 
+            }
+
+            animationEventTrigger.Value.OnEventTrigger -= OnEventTrigger;
+        }
+
+        private void OnEventTrigger(string sentEventName)
+        {
+            if (!sentEventName.Equals(eventName.Value, StringComparison.OrdinalIgnoreCase))
+                return;
+
+            animationEnded = true;
         }
 
         private void DecideAttackId()
