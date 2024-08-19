fdd82ce6429e69daef46aa34320a6ee0d3e99d22
Malakazamm <mgreg.music@gmail.com>
2024-08-19 16:54:04 -0400

    New Soundbanks

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,95 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMB_2D_DUNGEONATMOS = 4222994794U;
+        static const AkUniqueID PLAY_AMB_3D_CAVEDROPS = 779615162U;
+        static const AkUniqueID PLAY_BOS_ATK_FIREBALL = 1880719579U;
+        static const AkUniqueID PLAY_BOS_ATK_METEOR_IMPACT = 1710112781U;
+        static const AkUniqueID PLAY_BOS_ATK_METEOR_RUMBLE = 1875915164U;
+        static const AkUniqueID PLAY_BOS_ATK_SHOCKWAVE = 4255133681U;
+        static const AkUniqueID PLAY_BOS_DAMAGETAKEN = 724278647U;
+        static const AkUniqueID PLAY_BOS_DEATH = 2558195277U;
+        static const AkUniqueID PLAY_BOS_FOOTSTEPS = 2740614378U;
+        static const AkUniqueID PLAY_MENU_HOVER = 3835684012U;
+        static const AkUniqueID PLAY_MENU_SELECT = 2167056030U;
+        static const AkUniqueID PLAY_PLYR_ATTACK = 3156901378U;
+        static const AkUniqueID PLAY_PLYR_DAMAGETAKEN = 4153750570U;
+        static const AkUniqueID PLAY_PLYR_DEATH = 3433621352U;
+        static const AkUniqueID PLAY_PLYR_DODGE = 2145164375U;
+        static const AkUniqueID PLAY_PLYR_FOOTSTEPS = 673033979U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DENSITY_FA_SSGRAIN = 2715217995U;
+        static const AkUniqueID IMMERSION_FA_SSGRAIN = 2481728872U;
+        static const AkUniqueID INSTRUMENT_FA_SSGRAIN = 2317409760U;
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID PROXIMITY_FA_SSGRAIN = 1791284502U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID RPM_FA_SSGRAIN = 1656280998U;
+        static const AkUniqueID SIMULATION_FA_SSGRAIN = 2428833394U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMB_BANK = 1838976376U;
+        static const AkUniqueID BOS_BANK = 2366286276U;
+        static const AkUniqueID MENU_BANK = 3113887189U;
+        static const AkUniqueID MUS_BANK = 827800787U;
+        static const AkUniqueID PLYR_BANK = 3450962857U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID AMBIENT2D = 3950933845U;
+        static const AkUniqueID AMBIENT3D = 3934156282U;
+        static const AkUniqueID BOSS = 1560169506U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MENU = 2607556080U;
+        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PLAYER = 1069431850U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID CLOSEREVERB = 1193032327U;
+        static const AkUniqueID LONGREVERB = 4155604923U;
+        static const AkUniqueID REVERBS = 3545700988U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
